7bf55fcce1d7246ab09f161eec383f48ebb91c45
Marco Costa <mmarcottulio@gmail.com>
2018-04-12 16:57:28 -0400

    Add :instance

--- lib/rspec/arguments/monkey_patcher.rb
+++ lib/rspec/arguments/monkey_patcher.rb
@@ -12,9 +12,27 @@ module RSpec
       #
       def subject
         __memoized.fetch_or_store(:subject) do
-          described = described_class || self.class.metadata.fetch(:description_args).first
-          described.is_a?(Class) ? process_subject(described) : described
+          metadata = self.class.metadata
+          described = described_class || metadata.fetch(:description_args).first
+
+          if described.is_a?(Class)
+            if metadata[:rspec_arguments] || metadata[:method] || metadata[:class_method]
+              process_subject(described) { |instance| __memoized.fetch_or_store(:instance) { instance } }
+            else
+              described.new
+            end
+          else
+            described
+          end
         end
+      end
+
+      #
+      # Reference to the instantiated object, if testing an instance or instance method (but not class method).
+      #
+      def instance
+        subject # Ensures subject has been loaded
+        __memoized.fetch_or_store(:instance) { raise 'Instance is only available when testing class instances or instance methods' }
       end
     end
   end
